998b13649a7b0fdfdb7024b0defb491422da0d68
Abkeyev Daniyar <abkeyev@gmail.com>
2021-02-23 22:12:39 +0600

    build bugfix

--- pages/branches/index.tsx
+++ pages/branches/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Layout from "../../components/Layout";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import MenuItem from '@material-ui/core/MenuItem'
-import { Grid } from '@material-ui/core'
 import {
   BccTypography,
   BccChip,
